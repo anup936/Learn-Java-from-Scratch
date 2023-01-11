861107b52c5b6566befa22a7ff210fca6cc7b52b
Anup kumara yadav <73605555+anup936@users.noreply.github.com>
2023-01-11 22:37:54 +0530

    Data types in java #primitive
    8 types of primitive datatypes are present in java.

---
+++ Datatype.java
@@ -0,0 +1,19 @@
+public class Datatype {
+    public static void main(String args[]){
+        // 8 types of primitive data types in java
+        byte b = 8; //size=there is 256 numbers we can store [-128 to 127]
+        System.out.println(b);
+        char ch = 'a'; //size=2 bytes
+        System.out.println(ch);
+        boolean var = false;//size= 1 bytes
+        System.out.println(var);
+        /*float price = 20.5; ocupied size =4 bytes
+        int number = 34; ocupied size= 4 bytes
+        long =bdi value of integer ocupied size =8 bytes
+        double = is use for larg value of integer , ocupied size =8 bytes
+        short n = 240; ocupied size =2 bytes
+        */
+
+    }
+    
+}
---
+++ Pattern.java
@@ -0,0 +1,22 @@
+public class Pattern {
+    public static void main(String args[]){
+      /*System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    
+        int a = 10;
+        int b= 5;
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+        String name ="Anup";
+        System.out.println(name);
+        */
+        
+
+
+
+    }
+}

6216541c36aee87ee7d6cc6e8057928ebe80e7d8
Anup kumar yadav <73605555+anup936@users.noreply.github.com>
2023-01-14 16:14:09 +0530

    Operators in java.
    There are verious types of Operator like Arithmetic ,Logical, Relational, Assignment .

---
+++ Operator.java
@@ -0,0 +1,57 @@
+public class Operator {
+    public static void main(String args[] ){
+       //arithmetic operator
+        /*  int x = 2, y=4;
+        int z = x+ y;
+        System.out.print(z);
+
+        int x = 2, y=4;
+        int z = x- y;
+        System.out.print(z);
+
+        int x = 2, y=4;
+        int z = x* y;
+        System.out.print(z);
+
+        int x = 2, y=4;
+        int z = x/ y;
+        System.out.print(z);
+     
+
+        //relationl operator
+        int x = 2, y=4;
+        System.out.print(x== y);
+
+        int x = 2, y=4;
+        System.out.print(x!=y);
+
+        int x = 2, y=4;
+        System.out.print(x<y);
+
+        int x = 2, y=4;
+        System.out.print(x>y);
+
+        int x = 2, y=4;
+        System.out.print(x>=y);
+
+        int x = 2, y=4;
+        System.out.print(x<=y);
+
+        
+
+        // logical Operator
+        int x = 2, y=4;
+        System.out.print(x<y && x<y);
+
+        
+        int x = 2, y=4;
+        System.out.print(x>y || x<y);
+
+        int x = 2, y=4;
+        System.out.print(! (x<y));
+        */ 
+        // Assignment Operator
+        
+
+    }
+}
